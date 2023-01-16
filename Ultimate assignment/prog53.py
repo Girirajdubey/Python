c2d00b506529a0055b0cc45d6b97c9dcb284d45d
Dipanjon Dubey <115858630+Girirajdubey@users.noreply.github.com>
2023-01-16 11:14:36 +0530

    yo yo i have do something

--- Ultimate assignment/prog53.py
+++ Ultimate assignment/prog53.py
@@ -3,3 +3,14 @@ lst=[]
 for i in range(len(alpha)):
     lst+=[alpha[i]*(i+1)]
 print(lst)
+
+#another approach
+"""
+alpha="abcdefghijklmnopqrstuvwxyz"
+lst=[]
+for i in range(len(alpha)):
+    a=alpha[i]*(i+1)
+    #print(a)
+    lst.append(a)   #USING append
+print(lst)
+"""
